a7adb90a374ea45751d050df07470058a5085e24
jennywnuo <jennywnuo@gmail.com>
2020-07-28 13:29:27 -0700

    Initial Commit

---
+++ Deer Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deer Diary
+//
+//  Created by jenny nuo on 7/28/20.
+//  Copyright © 2020 jenny and clara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
