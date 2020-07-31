136912cf38a3882faae48866cc53eef7b0f3b529
Clara H <ch0976192558@gmail.com>
2020-07-31 22:51:04 +0800

    hallelujah!! theWorld works! // try to figure out how to add url + rounded corners.

---
+++ Deer Diary/EventCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventCollectionViewCell.swift
+//  Deer Diary
+//
+//  Created by Clara Hung on 7/31/20.
+//  Copyright © 2020 jenny and clara. All rights reserved.
+//
+
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var eventImage: UIImageView!
+    
+    @IBOutlet weak var eventName: UILabel!
+    
+    @IBOutlet weak var eventDescription: UILabel!
+    
+    
+    @IBAction func learnBtn(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://issuesintheworld.carrd.co/")!)
+    }
+}
--- Deer Diary/View Controllers/TheWorldViewController.swift
+++ Deer Diary/View Controllers/TheWorldViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class TheWorldViewController: UIViewController {
+class TheWorldViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let eventName = ["Crisis in Yemen", "Portland Protests", "Justice for Breonna Taylor", "Hong Kong at Risk", "Uighur Concentration Camps"]
+    
+    let eventImage = [UIImage(named: "yemen"), UIImage(named: "portland"), UIImage(named: "breonna"), UIImage(named: "hongkong"), UIImage(named: "uighurs")]
+    
+    let eventDescription = ["A brutal civil war is ravaging Yemen even amid COVID-19. Its people are among the most vulnerable to war and sickness.", "Peaceful protestors and journalists in Portland, Oregon are being tear-gassed and arreseted by federal officers.", "It's been months since EMT Breonna Taylor was shot and killed by police. Her killers? They're still walking free.", "New security laws by China are upending the very definition of democracy in Hong Kong. The CCP is cracking down on liberties like never before.", "China doesn't want the world to know that it is running Muslim concentration camps in Xinjiang. "]
 
     override func viewDidLoad() {
         super.viewDidLoad();
-        let attributes = [NSAttributedString.Key.font: UIFont(name: "Charter-Bold", size: 17)!]
-        
-        UINavigationBar.appearance().titleTextAttributes = attributes
-
-
-        // Do any additional setup after loading the view.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! EventCollectionViewCell
+        
+        cell.eventName.text = eventName[indexPath.row]
+        cell.eventImage.image = eventImage[indexPath.row]
+        cell.eventDescription.text = eventDescription[indexPath.row]
+        
+        cell.contentView.layer.cornerRadius = 4.0
+        cell.contentView.layer.borderWidth = 1.0
+        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+        cell.contentView.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.white.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        cell.layer.shadowRadius = 4.0
+        cell.layer.shadowOpacity = 1.0
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+        
+        
+        return cell
+    }
+    
+
     
 
     /*
